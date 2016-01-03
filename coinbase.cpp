ba7e2ca8b19bc9e68bbc3fe529266cfbade5d34f
David Lacroix <davidlacroix@Davids-MBP.tr.cgocable.ca>
2016-01-03 17:27:07 -0500

    API key

--- coinbase.cpp
+++ coinbase.cpp
@@ -2,6 +2,15 @@
 #include <QDebug>
 #include <QMessageBox>
 
+
+/*
+
+    lacroix.david1@gmail.com
+    Clé d'API : SBH5GeIntChyxpax
+    Clé d'API secrète : G1WGo4vRMRleNVEkssfuhs8fDpT3UQ8T
+
+ */
+
 CoinBase::CoinBase()
 {
     orderBookAddr = "https://api.exchange.coinbase.com/products/BTC-CAD/book?level=2";
@@ -18,7 +27,9 @@ void CoinBase::interpreterOrderBook(QNetworkReply* reply)
         msgBox.setText("Erreur lors de la requete : " + reply->errorString());
         msgBox.exec();
         return;
-    }
+        Clé d'API : SBH5GeIntChyxpax
+
+        Clé d'API secrète : G1WGo4vRMRleNVEkssfuhs8fDpT3UQ8T  }
 
     // Laleur de reply->readAll() se vide apres usage
     QString reponse = reply->readAll();
